e66f81c90f5ac7e25bb9c80d4b0d31e1436f8543
Exstaa <exstagithub@gmail.com>
2024-08-16 20:48:21 +0400

    transaction model.

---
+++ java/az/hafizrzazade/financemanagement/model/Transaction.java
@@ -0,0 +1,30 @@
+package az.hafizrzazade.financemanagement.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type; 
+    private Double amount;
+    private String category;
+    private LocalDate date;
+    private String description;
+}
