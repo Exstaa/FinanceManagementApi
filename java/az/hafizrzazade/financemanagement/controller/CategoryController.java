c6636abedfaaddafc44a3113f4d714cf7495cd64
Exstaa <exstagithub@gmail.com>
2024-08-16 20:50:56 +0400

    category api (crud operations)

---
+++ java/az/hafizrzazade/financemanagement/controller/CategoryController.java
@@ -0,0 +1,50 @@
+package az.hafizrzazade.financemanagement.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import az.hafizrzazade.financemanagement.model.Category;
+import az.hafizrzazade.financemanagement.repository.CategoryRepository;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @PostMapping
+    public ResponseEntity<Category> addCategory(@RequestBody Category category) {
+        return new ResponseEntity<>(categoryRepository.save(category), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+        return categoryRepository.findById(id)
+                .map(category -> new ResponseEntity<>(category, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        if (categoryRepository.existsById(id)) {
+            categoryRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
