503fed2004e6dae896d6bc63a58e3bf5f90a4ba7
Exstaa <exstagithub@gmail.com>
2024-08-16 20:47:44 +0400

    main class.

---
+++ java/az/hafizrzazade/financemanagement/FinanceManagementMainClass.java
@@ -0,0 +1,11 @@
+package az.hafizrzazade.financemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceManagementMainClass {
+    public static void main(String[] args) {
+    	SpringApplication.run(FinanceManagementMainClass.class, args);
+    }
+}
