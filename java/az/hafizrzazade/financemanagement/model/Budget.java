6c4cb03552247001820551bb43b826af6a02f5a5
Exstaa <exstagithub@gmail.com>
2024-08-16 20:47:57 +0400

    budget model.

---
+++ java/az/hafizrzazade/financemanagement/model/Budget.java
@@ -0,0 +1,34 @@
+package az.hafizrzazade.financemanagement.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "budgets")
+public class Budget {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    private Double amount;
+    private LocalDate startDate;
+    private LocalDate endDate;
+}
